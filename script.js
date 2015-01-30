d92057908f5e58be0aeab6247c1f6ca10a0552b3
atgrubb23 <grubba@onid.oregonstate.edu>
2015-01-30 00:54:08 -0700

    Finished assignment. Spent most of time hooking up Favorite! button to add to favorites ul in the DOM.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.gistsEntryArray = [];
 function getGists() {
   var numPages = document.getElementsByName('filterNumPages')[0].value;
   for (var i = 1; i <= numPages; i++) {
@@ -50,8 +51,8 @@ function Gists() {
 }
 
 function createGistsList(ul, gistsPage) {
-  var gistsEntry = new Gists();
   for (var prop in gistsPage) {
+    var gistsEntry = new Gists();
     gistsEntry.setId(gistsPage[prop].id);
     gistsEntry.setUrl(gistsPage[prop].url);
     gistsEntry.setDescription(gistsPage[prop].description);
@@ -73,7 +74,8 @@ function createGistsList(ul, gistsPage) {
 function createGistsEntries(ul, gistsEntry) {
   var nestedUl = document.createElement('ul');
   nestedUl.innerText = gistsEntry.getId();
-  for (i = 0; i < 3; i++) {
+  nestedUl.id = gistsEntry.getId();
+  for (var i = 0; i < 3; i++) {
     var li = document.createElement('li');
     if (i === 0) {
       var anchor = document.createElement('a');
@@ -95,42 +97,115 @@ function createGistsEntries(ul, gistsEntry) {
   var input = document.createElement('input');
   input.type = 'button';
   input.value = 'Favorite!';
-  input.onclick = 'saveFavoriteGists(gistsEntry)';
-  displayGistsEntries(ul, nestedUl, input);
+  input.onclick = function() {
+    saveFavoriteGists(gistsEntry);
+  }
+  displayGistsEntries(gistsEntry, ul, nestedUl, input);
 }
 
-function displayGistsEntries(ul, entry, button) {
+function displayGistsEntries(gistsEntry, ul, nestedUl, button) {
   var pythonChecked = document.getElementsByName('filterPython')[0].checked;
   var jsonChecked = document.getElementsByName('filterJSON')[0].checked;
   var javascriptChecked = document.getElementsByName('filterJavascript')[0].checked;
   var sqlChecked = document.getElementsByName('filterSQL')[0].checked;
   if (!pythonChecked && !jsonChecked && !javascriptChecked && !sqlChecked) {
-    ul.appendChild(entry);
-    entry.appendChild(button);
+    ul.appendChild(nestedUl);
+    nestedUl.appendChild(button);
     return;    
   }
   if (pythonChecked && gistsEntry.getLanguage() === 'Python') {
-    ul.appendChild(entry);
-    entry.appendChild(button);
+    ul.appendChild(nestedUl);
+    nestedUl.appendChild(button);    numEntries++;
     return;
   }
   if (jsonChecked && gistsEntry.getLanguage() === 'JSON') {
-    ul.appendChild(entry);
-    entry.appendChild(button);
+    ul.appendChild(nestedUl);
+    nestedUl.appendChild(button);
     return;
   }
   if (javascriptChecked && gistsEntry.getLanguage() === 'JavaScript') {
-    ul.appendChild(entry);
-    entry.appendChild(button);
+    ul.appendChild(nestedUl);
+    nestedUl.appendChild(button);
     return;
   }
   if (sqlChecked && gistsEntry.getLanguage() === 'SQL') {
-    ul.appendChild(entry);
-    entry.appendChild(button);
+    ul.appendChild(nestedUl);
+    nestedUl.appendChild(button);
     return;
   }
 }
 
 function saveFavoriteGists(gistsEntry) {
-  localStorage.setItem('id', gistsEntry.getId());
+  gistsEntryArray.push(gistsEntry);
+  var entry = JSON.stringify(gistsEntryArray);
+  localStorage.setItem('favorites', entry);
+  removeEntryFromList(gistsEntry);
+  createEntryInFavorites(gistsEntry);
+}
+
+function removeEntryFromList(gistsEntry) {
+  var ul = document.getElementById('gistsList');
+  var nestedUl = document.getElementById(gistsEntry.getId());
+  ul.removeChild(nestedUl);
+}
+
+function createEntryInFavorites(gistsEntry) {
+  var ul = document.getElementById('favoritesList');
+  var nestedUl = document.createElement('ul');
+  nestedUl.innerText = gistsEntry.getId();
+  nestedUl.id = gistsEntry.getId();
+  for (var i = 0; i < 3; i++) {
+    var li = document.createElement('li');
+    if (i === 0) {
+      var anchor = document.createElement('a');
+      anchor.href = gistsEntry.getUrl();
+      anchor.target = '_blank';
+      anchor.innerText = gistsEntry.getUrl();
+      li.appendChild(anchor);
+      nestedUl.appendChild(li);
+    }
+    else if (i === 1) {
+      li.innerText = 'Description: ' + gistsEntry.getDescription();
+      nestedUl.appendChild(li);
+    }
+    else if (i === 2) {
+      li.innerText = 'Language: ' + gistsEntry.getLanguage();
+      nestedUl.appendChild(li);
+    }
+  }
+  var input = document.createElement('input');
+  input.type = 'button';
+  input.value = 'Remove!';
+  input.onclick = function() {
+    removeEntryFromFavorites(gistsEntry);
+  }
+  ul.appendChild(nestedUl);
+  nestedUl.appendChild(input);
+}
+
+function removeEntryFromFavorites(gistsEntry) {
+  var ul = document.getElementById('favoritesList');
+  var nestedUl = document.getElementById(gistsEntry.getId());
+  ul.removeChild(nestedUl);
+  localStorage.removeItem('favorites');
+  for (var i = 0; i < gistsEntryArray.length; i++) {
+    if (gistsEntry.getId() === gistsEntryArray[i].getId()) {
+      gistsEntryArray.splice(i, 1);
+    }
+  }
+  var entry = JSON.stringify(gistsEntryArray);
+  localStorage.setItem('favorites', entry);
+}
+
+window.onload = function() {
+  var favorites = JSON.parse(localStorage.getItem('favorites'));
+  for (entry in favorites) {
+    var gistsEntry = new Gists();
+    gistsEntry.setId(favorites[entry].id);
+    gistsEntry.setUrl(favorites[entry].url);
+    gistsEntry.setDescription(favorites[entry].description);
+    gistsEntry.setLanguage(favorites[entry].language);
+    createEntryInFavorites(gistsEntry);
+    gistsEntryArray.push(gistsEntry);
+  }
 }
